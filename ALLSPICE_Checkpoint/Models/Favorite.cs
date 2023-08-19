8de921426cb6956504df7ae4f4a6f995de21a3db
Devryn <devryn.zub@gmail.com>
2023-08-18 17:25:11 -0700

    updated favorites

---
+++ ALLSPICE_Checkpoint/Models/Favorite.cs
@@ -0,0 +1,13 @@
+using ALLSPICE_Checkpoint_csharp.Models;
+
+namespace ALLSPICE_Checkpoint.Models;
+public class Favorite
+{
+      public int Id { get; set; }
+    public string AccountId { get; set; }
+    public int RecipeId { get; set; }
+}
+public class RecipeFavorite : Recipe
+{
+  public int FavoriteId { get; set; }
+}  
---
+++ ALLSPICE_Checkpoint/Services/FavoritesService.cs
@@ -0,0 +1,42 @@
+namespace ALLSPICE_Checkpoint.Services;
+public class FavoritesService
+{
+    private readonly FavoritesRepository _favoritesRepository;
+
+    public FavoritesService(FavoritesRepository favoritesRepository)
+    {
+        _favoritesRepository = favoritesRepository;
+    }
+
+    internal Favorite CreateFavorite(Favorite favoriteData)
+    {
+        int favoriteId = _favoritesRepository.CreateFavorite(favoriteData);
+        Favorite favorite = GetFavoriteById(favoriteId);
+        return favorite;
+    }
+    internal Favorite GetFavoriteById(int favoriteId)
+    {
+      Favorite favorite = _favoritesRepository.GetFavoriteById(favoriteId);
+      if(favorite == null)
+      {
+        throw new Exception("BAD ID");
+      }
+      return favorite;
+    }
+
+    internal List<RecipeFavorite> GetMyRecipeFavorites(string userId)
+    {
+        List<RecipeFavorite> recipes = _favoritesRepository.GetMyRecipeFavorites(userId);
+        return recipes;
+    }
+
+    internal void RemoveFavorite(int favoriteId, string userId)
+    {
+        Favorite foundFavorite = GetFavoriteById(favoriteId);
+        if(foundFavorite.AccountId != userId)
+        {
+          throw new Exception("Cannot delete a favorite that isn't yours.");
+        }
+        _favoritesRepository.RemoveFavorite(favoriteId);
+    }
+}
